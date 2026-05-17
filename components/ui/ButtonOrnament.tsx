21f79ad57fe460bb1aaa0db83fb4d83d7cf9d3fc
anousone <anousone9102@gmail.com>
2026-05-17 09:51:41 +0700

    fix(ui): rename button-ornament to -v2 to bust browser cache

--- components/ui/ButtonOrnament.tsx
+++ components/ui/ButtonOrnament.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function ButtonOrnament() {
   return (
     <Image
-      src="/scene/button-ornament.png"
+      src="/scene/button-ornament-v2.png"
       alt=""
       width={74}
       height={96}
